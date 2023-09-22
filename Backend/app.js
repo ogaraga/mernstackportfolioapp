698ab3cca520ce9cff2945d2d7b228037059603b
Ogaraga <raymond.ogaraga@gmail.com>
2023-09-22 19:39:28 +0100

    review

--- Backend/app.js
+++ Backend/app.js
@@ -3,6 +3,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const path = require('path')
 
 const PORT = process.env.PORT || 8080
 const connectionDb = require('./config/dbase');
@@ -16,7 +17,7 @@ connectionDb;
 //adding middlware
 app.use(cors({origin:'http://localhost:5173'}));
 app.use(express.json({ limit: '100mb' }));
-app.use(express.static(__dirname + '/Frontend/src'));
+app.use('/Frontend', express.static(path.join(__dirname, 'src')))
 app.use('/', Routes);  
 
 
