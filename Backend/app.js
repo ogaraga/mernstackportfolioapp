9fe5948e1b6d0e4a4223881c2d9327dd0af4657a
Ogaraga <raymond.ogaraga@gmail.com>
2023-09-22 20:16:24 +0100

    review

--- Backend/app.js
+++ Backend/app.js
@@ -17,7 +17,6 @@ connectionDb;
 //adding middlware
 app.use(cors({origin:'http://localhost:5173'}));
 app.use(express.json({ limit: '100mb' }));
-app.use(express.static(path.join(__dirname, 'public')))
 app.use('/', Routes);  
 
 
